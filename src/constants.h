242ffb95de470e0779744aa9b2e488d04950c393
Alexandre Arnt <qtgzmanager@gmail.com>
2024-03-05 22:07:20 -0300

    - Changed baiacu-doas program.

--- src/constants.h
+++ src/constants.h
@@ -7,7 +7,7 @@ const QString ctn_PKG_BIN = QStringLiteral("/usr/sbin/pkg");
 const QString ctn_PKG_ADD_BIN = QStringLiteral("/usr/sbin/pkg_add");
 const QString ctn_PKG_DELETE_BIN = QStringLiteral("/usr/sbin/pkg_delete");
 const QString ctn_PKG_INFO = QStringLiteral("/usr/sbin/pkg_info");
-const QString ctn_BAIACU_DOAS = QStringLiteral("/usr/local/lib/baiacu/baiacu-doas");
+const QString ctn_BAIACU_DOAS = QStringLiteral("/usr/local/bin/qt-sudo");
 const QString ctn_BAIACU_DOAS_PARAMS = QStringLiteral("-d");
 
 #endif // CONSTANTS_H
